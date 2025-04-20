d174fc723d483981233704b2023b0012cb5c569b
shinkawakami <snnsk.2002@gmil.com>
2025-04-20 17:26:19 +0900

    認証機能とユーザーごとのタスク管理

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -10,4 +10,9 @@ class Task extends Model
     use HasFactory;
     
     protected $fillable = ['title', 'description'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
