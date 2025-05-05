8e3267e5c08603a6385ffa8470fbd30c7c48f4ef
shinkawakami <snnsk.2002@gmil.com>
2025-05-05 18:21:06 +0900

    aaaa

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-////
+////  
 Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
 Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
 Route::post('tasks', [TaskController::class, 'store'])->name('tasks.store');
