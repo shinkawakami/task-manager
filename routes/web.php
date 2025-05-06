3ec45031b6b994e0153b82435cf4f246fea762dd
shinkawakami <snnsk.2002@gmil.com>
2025-05-06 09:20:32 +0900

    qqqqqq

--- routes/web.php
+++ routes/web.php
@@ -18,17 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-<<<<<<< HEAD
-////  
-Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
-Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
-Route::post('tasks', [TaskController::class, 'store'])->name('tasks.store');
-Route::get('tasks/{task}', [TaskController::class, 'show'])->name('tasks.show');
-Route::get('tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
-Route::put('tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
-Route::delete('tasks/{task}', [TaskControlller::class, 'destroy'])->name('tasks.destroy');
-//Route::resource('tasks', TaskController::class);
-=======
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', function () {
@@ -46,4 +35,3 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
->>>>>>> origin/feature/bootstrap-ui
